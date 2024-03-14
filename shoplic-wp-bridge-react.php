92f56a2633be81dda8130330b2f204c0607a389a
byeongin <byeongin@shoplic.kr>
2024-03-14 15:14:59 +0900

    Increase Version

--- shoplic-wp-bridge-react.php
+++ shoplic-wp-bridge-react.php
@@ -8,7 +8,7 @@
  * Requires PHP: 7.4
  * License:      GPL v2 or later
  * License URI:  https://www.gnu.org/licenses/gpl-2.0.html
- * Version:      0.0.1
+ * Version:      0.0.2
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
