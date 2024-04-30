cb27919783afbf4ef59f814e458dc6ee96ddb749
byeongin <byeongin@shoplic.kr>
2024-04-30 17:13:00 +0900

    update version

--- shoplic-wp-bridge-react.php
+++ shoplic-wp-bridge-react.php
@@ -8,7 +8,7 @@
  * Requires PHP: 7.4
  * License:      GPL v2 or later
  * License URI:  https://www.gnu.org/licenses/gpl-2.0.html
- * Version:      1.0.0
+ * Version:      1.1.0
  * GitHub Plugin URI: https://github.com/shoplic-kr/shoplic-wp-bridge-react
  */
 
